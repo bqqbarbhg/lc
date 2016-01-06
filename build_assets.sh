ada8d16e5c18c0a3410581a3a94bda393652d86a
bqqbarbhg <samuli.1995@hotmail.com>
2016-01-06 19:54:10 +0200

    Add build_assets.sh

---
+++ build_assets.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p data
+
+find assets/sprites -iname '*.png' | assets/tools/pack_sprites.exe data/atlas.json data/atlas.png assets/sprites/
+
+mkdir -p data/mp3
+assets/tools/convert_mp3.py assets/audio data/mp3
+find data/mp3 -iname '*.mp3' | assets/tools/pack_binary.exe data/audio.json data/audio.bin data/mp3/
+rm -rf data/mp3
+
