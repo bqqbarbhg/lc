5015f29ea5f5c54161d448e0ae02114178585d70
Teemu Pääkkönen <oamaok@gmail.com>
2016-01-09 23:13:37 +0200

    Add basic README, database models, routes

---
+++ README.md
@@ -0,0 +1,24 @@
+Läätikopteri
+============
+
+This gonna be nice yo.
+
+Configuring
+-----------
+
+Serverside configuration files are located in the `src/js/server/config/` directory.
+
+Database credentials are located in the `datasources.json` file.
+
+Installation
+------------
+
+ - Run `npm install`
+ - Run `bower install`
+ - Run `node --harmony src/js/server/migrate.js`
+
+Running
+-------
+
+To start the API server, run `node --harmony .`
+
--- src/js/server/server.js
+++ src/js/server/server.js
@@ -2,24 +2,46 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
 
-
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-  });
-};
+// start the server
+boot(app, path.resolve(__dirname, 'config'), err => {
+	if (err) throw err;
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, path.resolve(__dirname, 'config'), function(err) {
-  if (err) throw err;
+	if (require.main === module)
+		app.listen(() => {app.emit('started');});
+});
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+
+// models
+var Score = app.models.Score;
+var UserAgent = app.models.UserAgent;
+
+// autofetch params
+app.param('scoreId', (req, res, next, scoreId) => {
+	Score.findById(scoreId)
+	.then(score => {
+		req.score = score || {};
+		next();
+	}).catch(next);
+});
+
+// routes
+app.get('/s', function (req, res, next) {
+	Score.find()
+	.then(scores => {
+		res.json(scores).end();
+		next();
+	}).catch(next);
+});
+
+app.get('/s/:scoreId', function (req, res, next) {
+	res.json(req.score).end();
+	next();
+});
+
+app.get('/r/:scoreId', function (req, res, next) {
+
+	// TODO: fetch replay for score
+
 });
